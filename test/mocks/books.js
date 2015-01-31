6de4e143ab7d840101a549aaa74490e4a9631093
Ashley Williams <ashley@bocoup.com>
2015-01-30 20:02:28 -0500

    add original implementation of tests

---
+++ test/mocks/books.js
@@ -0,0 +1,14 @@
+var mock_resource = {
+  author_id: 1,
+  title: "Spacecats! An astronomical tutorial on building single page web applications with AngularJS",
+  date_published: "jan 1 2015"
+};
+var mock_update = {
+  author_id: 1,
+  title: "Spacecats! An astronomical tutorial on building single page web applications with AngularJS",
+  date_published: "jan 30 2015"
+};
+
+
+exports.mock_resource = mock_resource;
+exports.mock_update = mock_update;
