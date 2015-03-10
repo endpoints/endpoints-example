891f060fcfaaad71c2d84677f84d387d2356e8d8
Ashley Williams <ashley@bocoup.com>
2015-03-10 11:46:19 -0400

    replace schema with schema.js require statement in books module

--- src/modules/books/controller.js
+++ src/modules/books/controller.js
@@ -3,23 +3,6 @@ const Endpoints = require('endpoints');
 module.exports = new Endpoints.Controller({
   source: new Endpoints.BookshelfSource({
     model: require('./model'),
-    schema: {
-      properties: {
-        id: {
-          type: 'integer'
-        },
-        name: {
-          type: 'string'
-        },
-        date_of_birth: {
-          type: 'string',
-          format: 'date-time'
-        },
-        date_of_death: {
-          type: 'string',
-          format: 'date-time'
-        }
-      }
-    }
-  })
+    schema: require('./schema')
+   })
 });
