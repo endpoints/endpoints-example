fa720455d9f472667d3d7306d79ca98af75abe82
Tyler Kellen <tyler@sleekcode.net>
2015-03-13 10:31:05 -0400

    rename source to adapter

--- src/modules/chapters/controller.js
+++ src/modules/chapters/controller.js
@@ -1,7 +1,7 @@
 const Endpoints = require('endpoints');
 
 module.exports = new Endpoints.Controller({
-  source: new Endpoints.BookshelfSource({
+  adapter: new Endpoints.BookshelfAdapter({
     model: require('./model')
   })
 });
